f4d112ea6f54ec8896d68c0081866d93ba621af7
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-09-12 11:11:17 +0530

    done success page

--- src/components/Success.js
+++ src/components/Success.js
@@ -2,8 +2,17 @@ import React from 'react'
 
 const Success = () => {
     return (
-        <div>
-            <img src="https://drive.google.com/file/d/1iUy4p89Qon5-E7yR3TojHq5YBNcN_B-k/view?usp=sharing"/>
+        <div className="logo-container">
+            <img src="https://github.com/nilankh/otpverification/blob/master/Artboard%201.png?raw=true"/>
+        <div className="w">
+            <h1>Welcome to AdmitKard</h1>
+        </div>
+        <p>
+            In order to provide you with a custom experience
+        </p>
+        <p>We need to ask you a few questions</p>
+        <button className="ver">Get Started</button>
+        <p>*This will only take 5min.</p>
         </div>
     )
 }
